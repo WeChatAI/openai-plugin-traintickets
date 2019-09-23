d5ee382b2dfae23c29225f162bbfa826c2e30467
jonahnzhang(张宁) <jonahnzhang@tencent.com>
2019-09-23 19:41:25 +0800

    code clean

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -9,6 +9,7 @@ App({
       // appid: "VEgbxLa9kYqzGOzstdeSF3xDbkS9zK",
       appid: "P5Ot9PHJDechCYqDFAW1AiK6OtG3Ja",
       textToSpeech: true,
+      // guideList: ["玩末日生存游戏"],
       success: () => {
         // plugin.send({
         //   query: "你好",
