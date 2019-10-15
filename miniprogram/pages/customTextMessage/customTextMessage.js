965e13d9801df1b2588edc3b3c647ce372e79af6
jonahnzhang(张宁) <jonahnzhang@tencent.com>
2019-10-15 16:59:14 +0800

    update

--- miniprogram/pages/customTextMessage/customTextMessage.js
+++ miniprogram/pages/customTextMessage/customTextMessage.js
@@ -6,56 +6,85 @@ Component({
 
   data: {
     imageArray: [
-      {url: "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg1.png"},
-      {url: "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg2.png"},
-      {url: "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg3.png"},
-      {url: "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg4.png"},
-      {url: "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg5.png"}
+      {
+        url:
+          "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg1.png"
+      },
+      {
+        url:
+          "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg2.png"
+      },
+      {
+        url:
+          "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg3.png"
+      },
+      {
+        url:
+          "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg4.png"
+      },
+      {
+        url:
+          "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/bg5.png"
+      }
     ],
     imgArr: [
-      {url: 'https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/btn1.png'},
-      {url: 'https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/btn2.png'},
-      {url: 'https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/btn3.png'}
+      {
+        url:
+          "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/btn1.png"
+      },
+      {
+        url:
+          "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/btn2.png"
+      },
+      {
+        url:
+          "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/btn3.png"
+      }
     ],
-    url: '',
+    url: "",
     flag: false,
     list: []
   },
   lifetimes: {
-    ready: function () { 
-      let that = this
-      const result = this.properties.msg
-      if (result.content.indexOf('评分') !== -1) {
-        if (result.content.indexOf('100') !== -1) {
+    ready: function() {
+      let that = this;
+      const result = this.properties.msg;
+      const score = result.content.replace(/.*评分\s*[:：]\s*(\d*)/, "$1");
+      if (result.content.indexOf("评分") !== -1) {
+        if (result.content.indexOf("100") !== -1) {
           that.setData({
-            flag:true,
-            url: "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/win.png"
-          })
+            flag: true,
+            url:
+              "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/win.png"
+          });
         } else {
           that.setData({
-            flag:true,
-            url: "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/dead.png"
-          })
+            flag: true,
+            url:
+              "https://res.wx.qq.com/mmspraiweb_node/dist/static/openaiplugin/img/dead.png"
+          });
         }
-        return 
+        return;
       }
       // const clicklink = /<a.*href=["']weixin:\/\/bizmsgmenu.*msgmenucontent=([^&"'>]*).*msgmenuid=([^&"'>]*)["']>.*<\/a>/g;
       if (result.res.options && result.res.options.length !== 0) {
         result.res.options.forEach((item, index) => {
-          item.url = that.data.imgArr[index].url
-        })
+          item.url = that.data.imgArr[index].url;
+        });
         that.setData({
-          flag:true,
-          url: that.data.imageArray[Math.floor(Math.random()* that.data.imageArray.length)].url,
+          flag: true,
+          url:
+            that.data.imageArray[
+              Math.floor(Math.random() * that.data.imageArray.length)
+            ].url,
           list: result.res.options
-        })
+        });
       }
-      
     }
   },
   methods: {
-    choose:function(e) {
-      sendFun.getData(e.currentTarget.dataset.title)
+    choose: function(e) {
+      sendFun.getData(e.currentTarget.dataset.title);
     }
   }
 });
