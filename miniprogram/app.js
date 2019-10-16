b2be1a5398f30b1e51256d76c47331f6d6912954
v_hayang <v_hayang@tencent.com>
2019-10-16 22:08:34 +0800

    修改逻辑

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -14,6 +14,8 @@ App({
       // background: "url('https://openai.weixin.qq.com/mmspraiweb_node/dist/static/weather/qing.png')",
       guideCardHeight: 50,
       operateCardHeight: 120,
+      // history: true,
+      // historySize: 60,
       success: () => {
         // plugin.send({
         //   query: "你好",
